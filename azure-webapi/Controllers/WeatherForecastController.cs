d8617ab3886b2cfad7a0dc32885ae7cf069be0c4
Gokul <iamgokulnathanmcs@gmail.com>
2025-04-01 13:44:58 +0530

    Added application insights and azure keyvault in webapi

--- azure-webapi/Controllers/WeatherForecastController.cs
+++ azure-webapi/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Mvc;
 
 namespace azure_webapi.Controllers;
@@ -6,27 +7,51 @@ namespace azure_webapi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    private readonly IConfiguration _configuration; 
+ 
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
     {
         _logger = logger;
+        _configuration = config;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet(Name = "SayHello")]
+    public string SayHello()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return "Hellow azure web api!";
+    }
+
+    [HttpGet]
+    [Route("FetchKey/{key}")]
+    public IActionResult FetchKeyvaultData(string key)
+    {
+        if (string.IsNullOrEmpty(key)) return BadRequest();
+        var data = string.Empty;
+
+        try
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            data = _configuration[key];
+        }catch(Exception ex)
+        {
+            _logger.LogError("Keyvault connection failed");
+                return Ok("keyvault connection failed");
+        }
+        return Ok(data);
+    }
+
+    [HttpGet]
+    [Route("HitLogs/{cutomstring}")]
+    public IActionResult HitLogs(string cutomstring)
+    {
+        if (string.IsNullOrEmpty(cutomstring)) return BadRequest();
+
+
+        _logger.LogInformation("Learn : LogInformation" + cutomstring);
+        _logger.LogError("Learn : LogError" + cutomstring);
+        _logger.LogCritical("Learn : LogCritical" + cutomstring);
+        _logger.LogWarning("Learn : LogWarning" + cutomstring);
+
+        return Ok();
     }
 }
--- azure-webapi/Program.cs
+++ azure-webapi/Program.cs
@@ -1,9 +1,50 @@
+using Azure.Identity;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 Console.WriteLine($"Using port: {port}");
-builder.WebHost.UseUrls($"http://*:{port}");
+builder.WebHost.UseUrls($"https://*:{port}");
+
+// Get Key Vault URI from configuration
+var keyVaultUri = builder.Configuration["AzureKeyVault:VaultUri"];
+
+if (!string.IsNullOrEmpty(keyVaultUri))
+{
+    var environment = builder.Environment.EnvironmentName;
+    if (environment == "Production")
+    {
+        builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential());
+    }
+    else
+    {
+        builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri), new InteractiveBrowserCredential());
+    }
+}
+
+// Code to fetch appinsights connection string from keyVault.
+var appInsightsConnectionString = builder.Configuration[builder.Configuration["AzureKeyVault:appInsights"]];
+
+if (!string.IsNullOrEmpty(appInsightsConnectionString))
+{
+    builder.Services.AddApplicationInsightsTelemetry(options =>
+    {
+        options.ConnectionString = appInsightsConnectionString;
+    });
+
+    builder.Services.AddLogging(log =>
+    {
+        log.AddApplicationInsights();
+        log.AddFilter<ApplicationInsightsLoggerProvider>("", LogLevel.Information);
+    });
+}
+else
+{
+    Console.WriteLine("Application Insights connection string is not configured.");
+}
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -20,19 +61,16 @@ builder.Services.AddCors(options =>
     });
 });
 
-
 var app = builder.Build();
 Console.WriteLine("Hello World! This is a test for Azure Web API.");
 // Configure the HTTP request pipeline.
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
-app.UseCors(); 
+app.UseCors();
 
 app.MapControllers();
 
